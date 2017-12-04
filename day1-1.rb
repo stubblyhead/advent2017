24daa661bd59ffdc976d6f4c7181b487d6505f5c
Mike Stevenson <stubblyhead@gmail.com>
2017-12-03 21:50:03 -0800

    initial commit

---
+++ day1-1.rb
@@ -0,0 +1,20 @@
+number = ''
+File.open('./day1.input') do |input|
+  number = input.readline
+end
+digits = number.to_s.split('')
+sum = 0
+
+digits.each_index do |index|
+  if index < digits.length - 1
+    if digits[index] == digits[index + 1]
+      sum += digits[index].to_i
+    end
+  else
+    if digits[index] == digits[0]
+      sum += digits[index].to_i
+    end
+  end
+end 
+
+puts sum     
