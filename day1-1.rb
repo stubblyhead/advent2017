1bfcc6ec2bf59f9090f5fb7dc446cda1c6c9bb4a
Mike Stevenson <stubblyhead@gmail.com>
2017-12-03 22:11:56 -0800

    removing edits

--- day1-1.rb
+++ day1-1.rb
@@ -1,6 +1,6 @@
 number = ''
 File.open('./day1.input') do |input|
-  number = input.readline
+  number = input.readline.chomp
 end
 digits = number.to_s.split('')
 sum = 0
@@ -9,7 +9,6 @@ digits.each_index do |index|
   if index < digits.length - 1
     if digits[index] == digits[index + 1]
       sum += digits[index].to_i
-    end
   else
     if digits[index] == digits[0]
       sum += digits[index].to_i
