63524aa0737022e9ee7591bf1e17f4bef2736550
Mike Stevenson <Mike.Stevenson@us.logicalis.com>
2017-12-05 12:54:37 -0800

    part 1

---
+++ day5/1.rb
@@ -0,0 +1,21 @@
+offsets = []
+File.open('./input') do |file|
+  file.each_line do |line|
+    offsets.push(line.chomp.to_i)
+  end
+end
+
+def jump(offsets, position)
+  nextposition = position + offsets[position]
+  offsets[position] += 1
+  return [offsets, nextposition]
+end
+
+position = 0
+steps = 0
+while position < offsets.length
+  offsets, position = jump(offsets,position)
+  steps += 1
+end
+
+puts steps
