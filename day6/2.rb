c06591e0ee7f21be68ff36d031317e25c5bc68ba
Mike Stevenson <Mike.Stevenson@us.logicalis.com>
2017-12-06 20:37:23 -0800

    part 2

---
+++ day6/2.rb
@@ -0,0 +1,32 @@
+def redistribute(banks)
+  redistrib = banks.max
+  currentbank = banks.index(redistrib)
+  banks[currentbank] = 0
+  while redistrib != 0
+    currentbank += 1
+    #puts "adding to banks[#{currentbank % banks.length}]"
+    banks[currentbank % banks.length] += 1
+    #puts "banks[#{currentbank % banks.length}] is now #{banks[currentbank % banks.length]}"
+    #p banks
+    redistrib -= 1
+  end
+  banks
+end
+banks = []
+File.open('./input') do |file|
+  banks = file.readline.split(' ')
+end
+banks.each_index do |bank|
+  banks[bank] = banks[bank].to_i
+end
+
+states = []
+
+while true
+  nextstate = redistribute(banks)
+  if states.index(nextstate)
+    puts states.length - states.index(nextstate)
+    break
+  end
+  states.push(nextstate.clone)
+end
