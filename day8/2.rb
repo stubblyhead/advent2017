4722cc20ddbc89b8e1b98a72c67c85c057d4a17c
Mike Stevenson <Mike.Stevenson@us.logicalis.com>
2017-12-08 19:46:43 -0800

    part 2

---
+++ day8/2.rb
@@ -0,0 +1,26 @@
+instructions = []
+File.open('./input') do |file|
+  file.each_line { |line| instructions.push(line) }
+end
+
+registers = {}
+maxvalue = 0
+
+instructions.each do |i|
+  i.chomp!
+  modifier, condition = i.split(' if ')
+  modifier.sub!('inc', '+=')
+  modifier.sub!('dec', '-=')
+  registers[modifier.split(' ')[0]] = 0 unless registers[modifier.split(' ')[0]]
+  registers[condition.split(' ')[0]] = 0 unless registers[condition.split(' ')[0]]
+  words = modifier.split(' ')
+  words[0] = words[0].prepend('registers["').concat('"]')
+  modifier = words.join(' ')
+  words = condition.split(' ')
+  words[0] = words[0].prepend('registers["').concat('"]')
+  condition = words.join(' ')
+  eval(modifier) if eval(condition)
+  maxvalue = [maxvalue, registers.values.max].max
+end
+
+puts maxvalue
