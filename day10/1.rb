735ca01505cb483151c330986ede2b2e0bbe966a
Mike Stevenson <Mike.Stevenson@us.logicalis.com>
2017-12-11 10:32:22 -0800

    finished part 1

--- day10/1.rb
+++ day10/1.rb
@@ -14,4 +14,32 @@ class CircularArray < Array
     end
     i
   end
+
+  def twist(length)
+    subary = pinch(@current, length)
+    subary.reverse!
+    subary.each do |i|
+      self[@current % size] = i
+      @current += 1
+    end
+    @current += @skip
+    @skip += 1
+    @current %= size
+  end
 end
+
+lengths = []
+File.open('./input') do |file|
+  lengths = file.readline.chomp.split(',')
+end
+
+
+ary = CircularArray.new([])
+
+(0..255).each { |i| ary[i] = i}
+
+lengths.each do |i|
+  ary.twist(i.to_i)
+end
+
+puts ary[0] * ary[1]
