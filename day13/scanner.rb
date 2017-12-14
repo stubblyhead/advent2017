1050f4e8a367c0082b0337543872ef32e8cbbc70
Mike Stevenson <Mike.Stevenson@us.logicalis.com>
2017-12-13 22:38:34 -0800

    finished part 2

--- day13/scanner.rb
+++ day13/scanner.rb
@@ -11,23 +11,26 @@ scanners.each_index do |i|
   scanners[i] = 0 if scanners[i] == nil
 end
 
-#scanners = [3,2,0,0,4,0,4]
-
 def get_severity(scanners, delay=0)
+  caught = false
   severity = 0
   scanners.each_index do |i|
-    if (i + delay) % ((scanners[i] - 1) * 2)== 0
+    if (i + delay) % ((scanners[i] - 1) * 2)== 0 && scanners[i] != 0
       severity += scanners[i] * i
+      caught = true
     end
   end
-  severity
+  [severity, caught]
 end
 
 severity = nil
 delay = 0
-while severity != 0
-  severity = get_severity(scanners, delay)
-  puts "severity #{severity} at delay #{delay}" if delay == 0 or severity == 0
+caught = true
+
+while caught
+  severity, caught = get_severity(scanners, delay)
+  puts "severity #{severity} at delay #{delay} but caught" if (delay == 0 or severity == 0) && caught
+  puts "escaped at delay #{delay}!" unless caught
   delay += 1
 
 end
