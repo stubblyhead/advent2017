f1ca849fa29ec825f92dceb2a63f250016bb9b15
Mike Stevenson <Mike.Stevenson@us.logicalis.com>
2017-12-14 22:23:20 -0800

    added multiple requirement for next value

--- day15/generator.rb
+++ day15/generator.rb
@@ -1,25 +1,28 @@
 class Generator
   attr_reader :lsb, :value
 
-  def initialize(seed, factor)
+  def initialize(seed, factor, multiple)
     @value = seed
     @factor = factor
+    @multiple = multiple
   end
 
   def next_value
     @value = (@value * @factor) % 2147483647
     @lsb = @value.to_s(2)[-16..-1]
+    next_value unless @value % @multiple == 0
   end
 end
 
-gen_a = Generator.new(634,16807)
-gen_b = Generator.new(301,48271)
+gen_a = Generator.new(65,16807,4)
+gen_b = Generator.new(8921,48271,8)
 
 matchcount = 0
-(1..40000000).each do |i|
+(1..5).each do |i|
   gen_a.next_value
   gen_b.next_value
-  matchcount += 1 if gen_a.lsb == gen_b.lsb
+  #matchcount += 1 if gen_a.lsb == gen_b.lsb
+  print "#{gen_a.lsb}\n#{gen_b.lsb}\n\n"
 end
 
-puts matchcount
+#puts matchcount
