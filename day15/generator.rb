64622ddbf7314a71c0b1e1e8ff7e19f3118df8e0
Mike Stevenson <Mike.Stevenson@us.logicalis.com>
2017-12-14 22:35:49 -0800

    part 2

--- day15/generator.rb
+++ day15/generator.rb
@@ -14,15 +14,28 @@ class Generator
   end
 end
 
-gen_a = Generator.new(65,16807,4)
-gen_b = Generator.new(8921,48271,8)
+gen_a = Generator.new(634,16807,1)
+gen_b = Generator.new(301,48271,1)
 
 matchcount = 0
-(1..5).each do |i|
+(1..40000000).each do |i|
   gen_a.next_value
   gen_b.next_value
-  #matchcount += 1 if gen_a.lsb == gen_b.lsb
-  print "#{gen_a.lsb}\n#{gen_b.lsb}\n\n"
+  matchcount += 1 if gen_a.lsb == gen_b.lsb
+  puts "cycle #{i}" if i % 1000000 == 0
 end
 
-#puts matchcount
+puts "#{matchcount} matches after 40,000,000 cycles for part 1"
+
+gen_a = Generator.new(634,16807,4)
+gen_b = Generator.new(301,48271,8)
+
+matchcount = 0
+(1..5000000).each do |i|
+  gen_a.next_value
+  gen_b.next_value
+  matchcount += 1 if gen_a.lsb == gen_b.lsb
+  puts "cycle #{i}" if i % 500000 == 0
+end
+
+puts "#{matchcount} matches after 5,000,000 cycles for part 2"
