d46b73cf23034ad5658181361b52a14e4aad0360
Mike Stevenson <Mike.Stevenson@us.logicalis.com>
2017-12-16 11:19:05 -0800

    part 2

--- day16/dance.rb
+++ day16/dance.rb
@@ -6,7 +6,8 @@ class Dance
   end
 
   def spin(num)
-    @dancers = @dancers.drop(@dancers.length - num) + @dancers.take(@dancers.length - num)
+    length = @dancers.length
+    @dancers = @dancers.drop(length - num) + @dancers.take(length - num)
   end
 
   def exchange(a, b)
@@ -29,19 +30,29 @@ File.open('./input') do |file|
 end
 
 dance = Dance.new(('a'..'p').to_a)
-moves.each do |i|
-  case i[0]
-  when 's'
-    dance.spin(i[1..-1].to_i)
-  when 'x'
-    exch = i[1..-1].split('/')
-    dance.exchange(exch[0].to_i, exch[1].to_i)
-  when 'p'
-    partners = i[1..-1].split('/')
-    dance.partner(partners[0],partners[1])
+permutations = [dance.dancers.clone]
+match = false
+
+until match
+  moves.each do |i|
+    case i[0]
+    when 's'
+      dance.spin(i[1..-1].to_i)
+    when 'x'
+      exch = i[1..-1].split('/')
+      dance.exchange(exch[0].to_i, exch[1].to_i)
+    when 'p'
+      partners = i[1..-1].split('/')
+      dance.partner(partners[0],partners[1])
+    end
+  end
+  puts "first iteration: #{dance.dancers.join}" if permutations.length == 1
+
+  if permutations.index(dance.dancers)
+    match = true
+  else
+    permutations.push(dance.dancers.clone)
   end
 end
 
-print "final positions: "
-dance.dancers.each { |i| print i }
-print "\n"
+puts "one billionth iteration: #{permutations[1000000000 % permutations.length].join}"
