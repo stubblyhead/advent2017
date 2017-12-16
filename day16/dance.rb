b6ff25e247783446fb10aec0522623bc21e30237
Mike Stevenson <Mike.Stevenson@us.logicalis.com>
2017-12-15 23:33:51 -0800

    passed test cases

--- day16/dance.rb
+++ day16/dance.rb
@@ -1,18 +1,32 @@
-class Dance < Array
+class Dance
+  attr_reader :dancers
+
+  def initialize(dancers)
+    @dancers = dancers
+  end
+
   def spin(num)
-    self = self.drop(self.length - num) + self.take(self.length - num)
+    @dancers = @dancers.drop(@dancers.length - num) + @dancers.take(@dancers.length - num)
   end
 
   def exchange(a, b)
-    temp = self[a]
-    self[a] = self[b]
-    self[b] = temp
+    temp = @dancers[a]
+    @dancers[a] = @dancers[b]
+    @dancers[b] = temp
   end
 
   def partner(a,b)
-    idx_a = self.index(a)
-    idx_b = self.index(b)
-    excahnge(idx_a, idx_b)
+    idx_a = @dancers.index(a)
+    idx_b = @dancers.index(b)
+    exchange(idx_a, idx_b)
   end
 
 end
+
+dance = Dance.new(%w[a b c d e])
+
+dance.spin(1)
+dance.exchange(3,4)
+dance.partner('e','b')
+
+print dance.dancers
