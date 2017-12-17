b467b9da033281468793059f3352a0edb1259f80
Mike Stevenson <Mike.Stevenson@us.logicalis.com>
2017-12-17 12:11:38 -0800

    changed class around some, test case passing

--- day17/spinlock.rb
+++ day17/spinlock.rb
@@ -1,17 +1,23 @@
-class Buffer < Array
-  attr_reader :start, :stepsize
+class Buffer
+  attr_reader :start, :stepsize, :buffer
 
   def initialize(stepsize)
     @stepsize = stepsize
-    @self[0] = 0
+    @buffer = Array.new(1,0)
     @start = 0
   end
 
   def cycle
-    next_value = @self.length
-    next_position = (@start + @stepsize) % @self.length + 1
-    @self = @self.insert(next_position, next_value)
-    @start = @self.index(next_value)
+    next_value = @buffer.length
+    next_position = (@start + @stepsize) % @buffer.length + 1
+    @buffer = @buffer.insert(next_position, next_value)
+    @start = @buffer.index(next_value)
   end
+end
 
-  
+spinlock = Buffer.new(3)
+(1..9).each do |i|
+  spinlock.cycle
+end
+
+puts spinlock.buffer.join(' ')
