1787c46553437a4c3d37050e2812c4b233bd5309
Mike Stevenson <Mike.Stevenson@us.logicalis.com>
2017-12-17 16:09:08 -0800

    only need to keep track of what's in spot 1

--- day17/spinlock.rb
+++ day17/spinlock.rb
@@ -1,17 +1,17 @@
 class Buffer
-  attr_reader :start, :stepsize, :buffer
+  attr_reader :start, :stepsize, :buffersize, :spot_one
 
   def initialize(stepsize)
     @stepsize = stepsize
-    @buffer = Array.new(1,0)
     @start = 0
+    @buffersize = 1
+    @spot_one = nil
   end
 
   def cycle
-    next_value = @buffer.length
-    next_position = (@start + @stepsize) % @buffer.length + 1
-    @buffer = @buffer.insert(next_position, next_value)
-    @start = @buffer.index(next_value)
+    @buffersize += 1
+    @start = (@start + @stepsize) % @buffersize + 2
+    @spot_one = @buffersize if @start == 1
   end
 end
 
@@ -21,11 +21,7 @@ spinlock = Buffer.new(316)
   #puts spinlock.buffer[1]
 end
 
-puts spinlock.buffer[spinlock.start + 1]
-
-(2018..50000000).each do |i|
-  spinlock.cycle
-end
-
-position_zero = spinlock.buffer.index(0)
-puts spinlock.buffer[position_zero + 1]
+puts spinlock.spot_one
+#(2018..50000000).each do |i|
+#  spinlock.cycle
+#end
