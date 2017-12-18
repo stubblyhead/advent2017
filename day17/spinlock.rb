b31238d20303fe934b2d4a828086ca6a63d191d8
Mike Stevenson <Mike.Stevenson@us.logicalis.com>
2017-12-18 14:22:51 -0800

    was off by one earlier but whatever

--- day17/spinlock.rb
+++ day17/spinlock.rb
@@ -9,7 +9,6 @@ class Buffer
   end
 
   def cycle
-    @buffersize += 1
     @start = (@start + @stepsize) % @buffersize + 2
     @spot_one = @buffersize if @start == 1
   end
@@ -18,10 +17,13 @@ end
 spinlock = Buffer.new(316)
 (1..2017).each do |i|
   spinlock.cycle
-  #puts spinlock.buffer[1]
 end
 
-puts spinlock.spot_one
-#(2018..50000000).each do |i|
-#  spinlock.cycle
-#end
+index_one_value = 1
+next_index = 1
+(2..50_000_000).each do |i|
+  next_index = (next_index + 316) % i + 1
+  index_one_value = i if next_index == 1
+end
+
+puts index_one_value
