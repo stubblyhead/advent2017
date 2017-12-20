91bdcbb8584eafb75318770bd589b914a7706d70
Mike Stevenson <Mike.Stevenson@us.logicalis.com>
2017-12-19 21:48:28 -0800

    pretty sure it's working?

--- day18/twoplayers.rb
+++ day18/twoplayers.rb
@@ -15,21 +15,24 @@ class Duet
 
   def queue_add(value)
     @queue.push(value)
+    puts "put value #{value} into queue"
   end
 
   def snd(value)
     if value.class.to_s == 'Fixnum'
       @partner.queue_add(value)
     else
-      @partner.queue_add(value)
+      @partner.queue_add(@registers[value])
     end
     @values_sent += 1
   end
   def set(register, value)
     if value.class.to_s == 'Fixnum'
       registers[register] = value
+      puts "registers[#{register}] is now #{value}"
     else
       registers[register] = registers[value]
+      puts "registers[#{register}] is now #{registers[value]}"
     end
   end
 
@@ -60,6 +63,7 @@ class Duet
   def rcv(register)
     if @queue.length > 0
       registers[register] = @queue[0]
+      puts "put value #{queue[0]} into registers[#{register}]"
       @queue = @queue[1..-1]
       return true
     else
@@ -69,6 +73,7 @@ class Duet
 
   def jgz(value, offset)
     value = registers[value] unless value.class.to_s == 'Fixnum'
+    offset = registers[offset] unless offset.class.to_s == 'Fixnum'
     if value > 0
       return offset
     else
@@ -81,6 +86,7 @@ class Duet
     until @waiting
       inst = @instructions[@instruction_pointer]
       inst_incrementor = 1
+      puts inst
       parts = inst.split(' ')
       if parts[1].match(/\d+/)
         parts[1] = parts[1].to_i
@@ -106,6 +112,7 @@ class Duet
         end
       when 'jgz'
         inst_incrementor = jgz(parts[1], parts[2])
+        puts inst_incrementor
       end
       @instruction_pointer += inst_incrementor
     end
@@ -126,7 +133,9 @@ player_a.set_partner(player_b)
 player_b.set_partner(player_a)
 
 until player_a.waiting == true && player_b.waiting == true && player_a.queue.length == 0 && player_b.queue.length == 0
+  puts "player_a's turn"
   player_a.play
+  puts "player_b's turn"
   player_b.play
 end
 
