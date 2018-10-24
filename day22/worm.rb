c147ae88441745aba7b8ae6de3d2e0d13d67cbda
Mike Stevenson <Mike.Stevenson@us.logicalis.com>
2018-10-23 19:31:29 -0700

    added virus class and first try at moving around

---
+++ day22/worm.rb
@@ -0,0 +1,86 @@
+require 'matrix'
+require 'pry'
+
+class Virus
+  attr_reader :x, :y, :direction, :infections
+
+  def initialize(grid)
+    @x = (grid.length / 2)
+    @y = (grid.length / 2)
+    @direction = :up
+    @infections = 0
+  end
+
+  def turn_left()
+    case @direction
+    when :up
+      @direction = :left
+    when :left
+      @direction = :down
+    when :down
+      @direction = :right
+    when :right
+      @direction = :up
+    end
+  end
+
+  def turn_right()
+    case @direction
+    when :up
+      @direction = :right
+    when :right
+      @direction = :down
+    when :down
+      @direction = :left
+    when :left
+      @direction = :up
+    end
+  end
+
+  def move(grid)
+    case @direction
+    when :up0
+      @y -= 1
+    when :right
+      @x += 1
+    when :down
+      @y += 1
+    when :left
+      @x -= 1
+    end
+    if @x == -1
+      grid = Matrix.hstack(Matrix.column_vector(Array.new(grid.length) { '.' } , grid))
+      @x = 0
+    elsif @x == grid.length
+      grid = Matrix.hstack(grid, Matrix.column_vector(Array.new(grid.length) { '.' } ))
+    elsif @y == -1
+      grid = Matrix.vstack(Matrix.row_vector((Array.new(grid.length) { '.'} ), grid))
+      @y = 0
+    elsif @y == grid.length
+      grid = Matrix.vstack(grid, Matrix.row_vector(Array.new(grid.lenth) { '.' } ))
+    end
+    grid
+  end
+
+
+  def burst(grid)
+    if grid[@x, @y] = '#'
+      turn_right()
+      grid[@x, @y] = '.'
+    else
+      turn_left()
+      grid[@x, @y] = '#'
+      @infections += 1
+    end
+    grid = move(grid)
+
+    grid
+  end
+end
+
+input = Matrix[['.','.','#'],
+               ['#','.','.'],
+               ['.','.','.']]
+
+morris = Virus.new(input)
+p morris.burst(input)
